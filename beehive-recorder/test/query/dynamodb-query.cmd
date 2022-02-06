013a877753d001329ad23e70ba0745e491cf3076
antibug63 <42036082+antibug63@users.noreply.github.com>
2022-02-06 18:43:09 +0100

    Delete beehive-recorder directory

--- beehive-recorder/test/query/dynamodb-query.cmd
+++
@@ -1,9 +0,0 @@
-aws dynamodb get-item --table-name SensorData --key file://test/query/key.json --output json
-
-aws dynamodb query --table-name SensorData \
-    --key-condition-expression "dev_id=:dev AND #ts>=:ts" \
-    --expression-attribute-names "#ts=timestamp" \
-    --expression-attribute-values file://test/query/query.json \
-    --projection-expression "#ts,payload_fields.sensor,metadata.data_rate" \
-    --output json
-
